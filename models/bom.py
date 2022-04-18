65430adc8f0c670e763d8f443e849e91299dc262
cfwuchang <67139173+cfwuchang@users.noreply.github.com>
2022-04-18 16:12:56 +0800

    Update bom.py

--- models/bom.py
+++ models/bom.py
@@ -3,4 +3,4 @@ from odoo import api, fields, models, tools, _
 class Bom(models.Model):
     _inherit = 'mrp.bom.line'
 
-    x_category = fields.Char(string=u'类别',related='product_id.categ_id.name')
+    x_category = fields.Char(string=u'产品类别',related='product_id.categ_id.name')
